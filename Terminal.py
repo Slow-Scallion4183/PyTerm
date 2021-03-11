2b07b1f65451a9290bbd4b284cfd75f8fa5b04b2
Lukas Does Dev <51633285+LukasDoesDev@users.noreply.github.com>
2021-03-11 10:23:46 +0200

    Update Terminal.py

--- Terminal.py
+++ Terminal.py
@@ -1,16 +1,42 @@
 # This Is A Terminal For Windows Made In Python
 
+import subprocess
 import os
-import getpass
-from os import system
+import platform
 
-system("title " + "Terminal v0.1")
+os.system('') # allows colours to be printed in Command Prompt and Powershell natively
 
-print("Terminal 0.1 By SkepticalPotato2k And LukasDoesDev")
+class colors:
+    BLACK = '\033[30m'
+    RED = '\033[31m'
+    GREEN = '\033[32m'
+    YELLOW = '\033[33m'
+    BLUE = '\033[34m'
+    MAGENTA = '\033[35m'
+    CYAN = '\033[36m'
+    WHITE = '\033[37m'
+    UNDERLINE = '\033[4m'
+    RESET = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+user = subprocess.run(['whoami'], capture_output=True).stdout.decode('UTF-8').strip()
+coloredUser = f'{colors.YELLOW}{user}'
+
+hostname = subprocess.run(['hostname'], capture_output=True).stdout.decode('UTF-8').strip()
+coloredHostname = f'{colors.BLUE}{hostname}'
+
+
+fullPrompt = f'{colors.BOLD}{colors.RED}[{coloredUser}{colors.GREEN}@{coloredHostname}{colors.RED}]{colors.WHITE}$ {colors.RESET}'
+
+if platform() == "Windows":
+    system("title " + "Terminal v0.1") # Does not work on Linux
+
+print("Terminal v0.1 By SkepticalPotato2k And LukasDoesDev")
 
 while True:
     print()
-    commandInput = input(getpass.getuser() + "@Terminal $ ")
+    commandInput = input(prompt)
     
     if commandInput == 'exit':
         break
@@ -23,7 +49,7 @@ while True:
                 # Remove the "@eval" from the start
                 raw.pop(0)
                 # Join the list together to create the script location
-                script = raw.join(' ')
+                script = ' '.join(raw)
 
                 # Open for reading
                 scriptFile = open(script, 'r')
